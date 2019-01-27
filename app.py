5ead96a17302fd5f49bd1363df075c24662c84e6
MarvinTaschenberger <marvin.taschenberger@gmail.com>
2019-01-27 19:33:39 +0100

    added minimal authentification

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-
+import os
+import pathlib as pl
 import pandas as pd
 import flask
 import dash
+import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table as dt
 
-server = flask.Flask(__name__)
-app = dash.Dash(__name__, server=server)
-app.config.suppress_callback_exceptions = True
+import dash
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+VALID_USERNAME_PASSWORD_PAIRS = [
+    ("data-solutions", "awesome"),
+    ("complex", "marvelous")
+]
+
+static_image_route = 'static/'
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets,server=server)
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+app.config.suppress_callback_exceptions = True
 
 def indicator(color, text, id_value):
     return html.Div(
@@ -20,6 +34,7 @@ def indicator(color, text, id_value):
                 className="twelve columns indicator_text"
             ),
             html.P(
+                children="INIT",
                 id=id_value,
                 className="indicator_value"
             ),
@@ -28,7 +43,21 @@ def indicator(color, text, id_value):
         style={"background-color": color}
     )
 
-
+def indicator_graph(color, text, id_value):
+    return html.Div([
+        html.P(
+            text,
+            className="twelve columns indicator_text"
+        ),
+        dcc.Graph(
+            id=id_value,
+            style={"height": "100%", "width": "98%"},
+            config=dict(displayModeBar=False)
+        )
+    ],
+        className="four columns indicator",
+        style={"background-color": color}
+    )
 def report_table(table: pd.DataFrame, subset: list, mark: str ):
     df = table[subset]
     cols = df.drop(mark, axis=1).columns
@@ -49,18 +78,19 @@ def report_table(table: pd.DataFrame, subset: list, mark: str ):
         style={"background-color": "#ffffff", "margin-top": "5%"}
     )
 
-def dash_tabel(df):
+def dash_tabel(data, subset):
+    df = data[subset]
     return dt.DataTable(
         data=df.to_dict('rows'),
         columns=[{'id': c, 'name': c} for c in df.columns],
         n_fixed_rows=1,
-        style_as_list_view=True,
-
-        style_table={
-            'maxHeight': '1000',
-            'overflowY': 'scroll'
-        },
+        style_as_list_view=False,
+        style_cell={'textAlign': 'left'},
         style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(248, 248, 248)'
+            },
             {
                 'if': {
                     'column_id': 'status',
@@ -95,3 +125,6 @@ def dash_tabel(df):
             },
         ]
     )
+
+def color_state(state):
+    return {"color":"#2a9c3c"}
